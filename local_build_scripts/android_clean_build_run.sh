f4dd94bcff539b610aa3ba79e602959ba50998d7
alepar <76537532+alepar666@users.noreply.github.com>
2025-08-27 13:31:21 +0200

    added local build script

--- local_build_scripts/android_clean_build_run.sh
+++ local_build_scripts/android_clean_build_run.sh
@@ -3,7 +3,6 @@ cordova plugin remove cordova-plugin-foreground-play
 cordova platform remove android
 cordova clean
 cordova platform add android
-cordova plugin add /YOUR_PATH/cordova-plugin-foreground-play
+cordova plugin add ./cordova-plugin-foreground-play
 cordova prepare android
 cordova build android
-cordova run android
