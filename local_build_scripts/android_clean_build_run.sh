54925f77e0cfbc91a473f1486cca1e4dc087028a
Pander <pander@users.sourceforge.net>
2025-09-08 14:10:37 +0200

    added fastlane generation

--- local_build_scripts/android_clean_build_run.sh
+++ local_build_scripts/android_clean_build_run.sh
@@ -1,4 +1,4 @@
-# use this for android clean build process
+# use this for Android clean build process
 cordova plugin remove cordova-plugin-foreground-play
 cordova platform remove android
 cordova clean
